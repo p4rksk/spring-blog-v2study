3af7ceeb076d0243b13ebf6610ba3a2f103f2fb2
yeeun0118s <yeeun0118s@naver.com>
2024-03-18 01:54:51 +0900

    게시글 목록보기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,22 +1,27 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
-    private final BoardRepository boardRepository;
+    private final BoardNativeRepository boardNativeRepository;
     @PostMapping("/board/save")
     public String save(String title, String content, String username){
-        boardRepository.save(username,content,title);
+        boardNativeRepository.save(username,content,title);
         return "redirect:/";
     }
     @GetMapping("/" )
-    public String index() {
+    public String index(HttpServletRequest req) {
+        List<Board> boardList = boardNativeRepository.findAll();
+        req.setAttribute("boardList",boardList);
         return "index";
     }
 
--- src/main/java/shop/mtcoding/blog/board/BoardNativeRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardNativeRepository.java
@@ -4,10 +4,20 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Repository
-public class BoardRepository {
+public class BoardNativeRepository {
     private final EntityManager em;
+
+    public List<Board>findAll(){
+        String q = """
+                select * from board_tb order by id desc
+                """;
+        List<Board> boardList = em.createNativeQuery(q,Board.class).getResultList();
+        return boardList;
+    }
 
     public void save(String username, String title, String content) {
         String q = """
@@ -19,5 +29,6 @@ public class BoardRepository {
                 .setParameter(3, content)
                 .executeUpdate();
     }
+
 }
 
